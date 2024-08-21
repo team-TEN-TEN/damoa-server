ae1af12c037422d4618d835c383b71058ba8afff
Lee ChanMi <anytime0224@gmail.com>
2024-08-21 13:20:04 +0900

    [init] 프로젝트 초기세팅
    - 배포 환경별 설정파일 세팅
    - 기본 명세서 툴 swagger 설정
    - gitignore
    - 기본 의존성 추가

---
+++ src/main/java/com/tenten/damoa/common/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.tenten.damoa.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Damoa Server API Docs"));
+    }
+}
+
