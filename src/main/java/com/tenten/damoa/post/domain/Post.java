1397d76200a7043e9743bb662e04cb90adec935c
YuGyeong98 <kuk3097@naver.com>
2024-08-23 18:46:19 +0900

    #15 [feat] post 엔티티, 레포지토리 생성

---
+++ src/main/java/com/tenten/damoa/post/domain/Post.java
@@ -0,0 +1,42 @@
+package com.tenten.damoa.post.domain;
+
+import com.tenten.damoa.common.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String contentId;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private SnsType type;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private int viewCount;
+
+    @Column(nullable = false)
+    private int likeCount;
+
+    @Column(nullable = false)
+    private int shareCount;
+}
