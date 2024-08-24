d5e1ad4d8223fdc18a73754a43b4f84c0425f88f
Lee ChanMi <anytime0224@gmail.com>
2024-08-24 11:43:59 +0900

    [feat] 예외 처리 시 에러가 발생한 항목에 대한 세부사항을 응답에 추가

--- src/main/java/com/tenten/damoa/common/exception/ErrorCode.java
+++ src/main/java/com/tenten/damoa/common/exception/ErrorCode.java
@@ -12,8 +12,9 @@ public enum ErrorCode {
      * 400 - Bad Request
      */
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다. 입력값을 확인하고 다시 시도해주세요."),
-    METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION(HttpStatus.BAD_REQUEST, "요청 값의 타입이 잘못되었습니다."),
-    INVALID_FORMAT_EXCEPTION(HttpStatus.BAD_REQUEST, "요청 값이 유효하지 않은 데이터입니다."),
+    ARGUMENT_TYPE_MISMATCH_EXCEPTION(HttpStatus.BAD_REQUEST, "입력 값이 예상된 형식이나 유형과 일치하지 않습니다."),
+    METHOD_ARGUMENT_VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "입력된 값이 유효하지 않습니다. 각 파라미터의 조건을 확인해 주세요."),
+    INVALID_FORMAT_EXCEPTION(HttpStatus.BAD_REQUEST, "요청된 데이터의 형식이 잘못되었습니다. 유효한 JSON 형식을 사용해 주세요."),
     MISSING_PARAMETER_EXCEPTION(HttpStatus.BAD_REQUEST, "필수 요청 값이 누락되었거나 잘못되었습니다."),
 
     /**
--- src/main/java/com/tenten/damoa/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tenten/damoa/common/exception/GlobalExceptionHandler.java
@@ -1,16 +1,14 @@
 package com.tenten.damoa.common.exception;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
-import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import java.util.List;
-import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSourceResolvable;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.ServletRequestBindingException;
@@ -25,7 +23,6 @@ public class GlobalExceptionHandler {
 
     private static final String ERROR_LOG_MESSAGE = "[ERROR] {} : {}";
     private static final String WARN_LOG_MESSAGE = "[WARN] {} : {}";
-
 
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<ErrorResponse<Void>> handleBusinessException(
@@ -46,31 +43,45 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(BindException.class)
-    protected ResponseEntity<ErrorResponse<List<String>>> handleBindException(
-            final BindException e) {
+    protected ResponseEntity<ErrorResponse<List<String>>> handleBindException(final BindException e) {
         log.warn(ERROR_LOG_MESSAGE, e.getClass().getSimpleName(), e.getMessage());
 
-        List<String> details = getBindingResultErrorMessages(e.getBindingResult());
+        List<String> details = e.getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
 
         return ErrorResponse.toResponseEntity(ErrorCode.BAD_REQUEST, details);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ErrorResponse<List<String>>> handleBindException(
-            final MethodArgumentNotValidException e) {
+    protected ResponseEntity<ErrorResponse<List<String>>> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
         log.warn(ERROR_LOG_MESSAGE, e.getClass().getSimpleName(), e.getMessage());
 
-        List<String> details = getBindingResultErrorMessages(e.getBindingResult());
+        List<String> details = e.getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
 
         return ErrorResponse.toResponseEntity(ErrorCode.BAD_REQUEST, details);
     }
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    protected ResponseEntity<ErrorResponse<List<String>>> handleConstraintViolationException(
-            final ConstraintViolationException e) {
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    protected ResponseEntity<ErrorResponse<List<String>>> handleHandlerMethodValidationException(final HandlerMethodValidationException e) {
         log.warn(ERROR_LOG_MESSAGE, e.getClass().getSimpleName(), e.getMessage());
 
-        List<String> details = getConstraintViolationErrorMessages(e.getConstraintViolations());
+        List<String> detail = e.getAllErrors().stream()
+                .map(MessageSourceResolvable::getDefaultMessage)
+                .toList();
+
+        return ErrorResponse.toResponseEntity(ErrorCode.METHOD_ARGUMENT_VALIDATION_FAILED, detail);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    protected ResponseEntity<ErrorResponse<List<String>>> handleConstraintViolationException(final ConstraintViolationException e) {
+        log.warn(ERROR_LOG_MESSAGE, e.getClass().getSimpleName(), e.getMessage());
+
+        List<String> details = e.getConstraintViolations().stream()
+                .map(violation -> violation.getPropertyPath() + ": " + violation.getMessage())
+                .toList();
 
         return ErrorResponse.toResponseEntity(ErrorCode.BAD_REQUEST, details);
     }
@@ -80,15 +91,12 @@ public class GlobalExceptionHandler {
             final MethodArgumentTypeMismatchException e) {
         log.warn(ERROR_LOG_MESSAGE, e.getClass().getSimpleName(), e.getMessage());
 
-        return ErrorResponse.toResponseEntity(ErrorCode.METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION);
+        return ErrorResponse.toResponseEntity(ErrorCode.ARGUMENT_TYPE_MISMATCH_EXCEPTION);
     }
 
     @ExceptionHandler({
             HttpMessageNotReadableException.class,
-            InvalidFormatException.class,
-            ServletRequestBindingException.class,
-            MissingServletRequestParameterException.class,
-            HandlerMethodValidationException.class
+            InvalidFormatException.class
     })
     protected ResponseEntity<ErrorResponse<Void>> handleInvalidFormatException(final Exception e) {
         log.warn(ERROR_LOG_MESSAGE, e.getClass().getSimpleName(), e.getMessage());
@@ -104,18 +112,5 @@ public class GlobalExceptionHandler {
         log.warn(ERROR_LOG_MESSAGE, e.getClass().getSimpleName(), e.getMessage());
 
         return ErrorResponse.toResponseEntity(ErrorCode.MISSING_PARAMETER_EXCEPTION);
-    }
-
-    private List<String> getBindingResultErrorMessages(BindingResult bindingResult) {
-        return bindingResult
-                .getAllErrors().stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .toList();
-    }
-
-    private List<String> getConstraintViolationErrorMessages(Set<ConstraintViolation<?>> constraintViolations) {
-        return constraintViolations.stream()
-                .map(violation -> violation.getPropertyPath() + ": " + violation.getMessage())
-                .toList();
     }
 }
