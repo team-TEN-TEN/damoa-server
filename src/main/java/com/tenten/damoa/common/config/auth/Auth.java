b5896cfd231351018f06c44b036499f3f2dfbc95
YuGyeong98 <kuk3097@naver.com>
2024-08-26 00:35:54 +0900

    #18 [feat] jwt 인터셉터 구현
    - 헤더에 access token이 없을 경우
    - 유효하지 않은 토큰인 경우
    - 서비스 회원이 아닌 경우(PRE_MEMBER)

---
+++ src/main/java/com/tenten/damoa/common/config/auth/Auth.java
@@ -0,0 +1,14 @@
+package com.tenten.damoa.common.config.auth;
+
+import com.tenten.damoa.member.domain.MemberRole;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Auth {
+
+    MemberRole role() default MemberRole.PRE_MEMBER;
+}
