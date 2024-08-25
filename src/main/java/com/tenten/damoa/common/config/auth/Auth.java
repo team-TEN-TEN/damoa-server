55845d1daa64f8f71e31f45bb649c7dba16d991c
YuGyeong98 <kuk3097@naver.com>
2024-08-26 00:54:09 +0900

    #18 [fix] member role 기본값 MEMBER로 변경

--- src/main/java/com/tenten/damoa/common/config/auth/Auth.java
+++ src/main/java/com/tenten/damoa/common/config/auth/Auth.java
@@ -10,5 +10,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Auth {
 
-    MemberRole role() default MemberRole.PRE_MEMBER;
+    MemberRole role() default MemberRole.MEMBER;
 }
