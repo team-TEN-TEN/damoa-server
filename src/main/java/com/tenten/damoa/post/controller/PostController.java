50a7ae4c51283e2f7bfc32f733b39c99df710ce2
hyunkyung <sybaik98@gmail.com>
2024-08-25 15:01:26 +0900

    [#27] 게시물 상세 조회_모든 -드값을 반환

---
+++ src/main/java/com/tenten/damoa/post/controller/PostController.java
@@ -0,0 +1,25 @@
+package com.tenten.damoa.post.controller;
+
+import com.tenten.damoa.post.domain.Post;
+import com.tenten.damoa.post.service.PostDetailService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PostController {
+    private final PostDetailService postDetailService;
+
+    public PostController (PostDetailService postDetailService) {
+        this.postDetailService = postDetailService;
+    }
+
+    @GetMapping("/{id}/detail")
+    public ResponseEntity<Post> getPostDetail(@PathVariable("id") Long id) {
+        Post postDetail = postDetailService.getPostDetail(id);
+        return ResponseEntity.ok(postDetail);
+    }
+}
---
+++ src/main/java/com/tenten/damoa/post/service/PostDetailService.java
@@ -0,0 +1,37 @@
+package com.tenten.damoa.post.service;
+
+import com.tenten.damoa.common.exception.BusinessException;
+import com.tenten.damoa.common.exception.ErrorCode;
+import com.tenten.damoa.interaction.domain.InteractionCategory;
+import com.tenten.damoa.interaction.domain.InteractionHistory;
+import com.tenten.damoa.interaction.repository.InteractionHistoryRepository;
+import com.tenten.damoa.post.domain.Post;
+import com.tenten.damoa.post.repository.PostRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class PostDetailService {
+
+    private final PostRepository postRepository;
+
+    private final InteractionHistoryRepository interactionHistoryRepository;
+
+    public PostDetailService(PostRepository postRepository, InteractionHistoryRepository interactionHistoryRepository) {
+        this.postRepository = postRepository;
+        this.interactionHistoryRepository = interactionHistoryRepository;
+    }
+    @Transactional
+    public Post getPostDetail(Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(()-> new BusinessException(ErrorCode.BAD_REQUEST));
+
+
+        post.setUpdatedAt(LocalDateTime.now());
+        postRepository.save(post);
+        return post;
+
+    }
+}
