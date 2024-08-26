48c65045900e93823fbb727f2fb6d9ce014df997
Lee ChanMi <anytime0224@gmail.com>
2024-08-26 14:17:40 +0900

    #22 [doc] 해시태그 통계 응답 객체 명세서 작성

--- src/main/java/com/tenten/damoa/stat/controller/dto/HashtagStatRes.java
+++ src/main/java/com/tenten/damoa/stat/controller/dto/HashtagStatRes.java
@@ -1,11 +1,16 @@
 package com.tenten.damoa.stat.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Builder;
 
+@Schema(description = "해시태그 통계 응답 객체")
 @Builder
 public record HashtagStatRes(
+        @Schema(description = "일자")
         LocalDateTime date,
+
+        @Schema(description = "값")
         Integer value
 ) {
 }
