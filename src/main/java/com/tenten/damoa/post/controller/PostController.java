963e46424a01aeb9ebd5be8b06a38c161d004f52
hyunkyung <sybaik98@gmail.com>
2024-08-26 14:14:10 +0900

    [etc] swagger 설명 추가

--- src/main/java/com/tenten/damoa/post/controller/PostController.java
+++ src/main/java/com/tenten/damoa/post/controller/PostController.java
@@ -2,12 +2,13 @@ package com.tenten.damoa.post.controller;
 
 import com.tenten.damoa.post.domain.Post;
 import com.tenten.damoa.post.service.PostDetailQueryService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Tag(name =  "게시물 조회", description = "id를 통해 게시물을 조회하는 API")
 @RestController
 @RequestMapping("/api/v1")
 public class PostController {
