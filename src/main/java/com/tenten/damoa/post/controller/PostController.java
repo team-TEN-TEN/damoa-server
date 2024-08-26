47198cdfed9155ce847b0d8cd79e4e4464fb025d
GiYeons <65033360+GiYeons@users.noreply.github.com>
2024-08-26 20:52:16 +0900

    [fix] 게시물 목록 반환시 ResponseEntity 적용

--- src/main/java/com/tenten/damoa/post/controller/PostController.java
+++ src/main/java/com/tenten/damoa/post/controller/PostController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +21,7 @@ public class PostController {
     @GetMapping("/posts")
     @Tag(name = "post", description = "게시글 API")
     @Operation(summary = "게시물 목록 조회 API", description = "쿼리 파라미터에 따라 조건에 맞는 게시물을 반환합니다.")
-    public Page<PostQueryRes> getPosts(@RequestParam(required = false) String tag,
+    public ResponseEntity<Page<PostQueryRes>> getPosts(@RequestParam(required = false) String tag,
                                        @RequestParam(required = false) String type,
                                        @RequestParam(name = "order-by", required = false, defaultValue = "createdAt") String orderBy,
                                        @RequestParam(required = false, defaultValue = "desc") String order,
@@ -29,7 +30,9 @@ public class PostController {
                                        @RequestParam(name = "page-size", required = false, defaultValue = "10") int pageSize,
                                        @RequestParam(required = false, defaultValue = "0") int page) {
 
-        return postQueryService.getPosts(tag, type, orderBy, order, searchBy, search, pageSize, page);
+        Page<PostQueryRes> ResPages = postQueryService.getPosts(tag, type, orderBy, order, searchBy, search, pageSize, page);
+        return ResponseEntity.ok(ResPages);
+
     }
 
 }
